78a7b926d1d78d1c8c2fadd2f8c483ed9c095598
Ryan Freng <ryanfreng@gmail.com>
2012-10-29 00:03:29 -0500

    fix routes to be at root level

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,56 +6,63 @@ describe "Static pages" do
 
   describe  "Home page" do
     before(:each) do
-      visit '/static_pages/home'
+      visit root_path
     end
 
     it "should have the h1 'Sample App'" do
-      page.should have_selector('h1', :text => 'Sample App')
+      page.should have_selector('h1', text: 'Sample App')
     end
 
     it  "should have the base title" do
-      page.should have_selector('title', :text => "#{base_title}")
+      page.should have_selector('title', text: "#{base_title}")
     end
     
     it "should not have a custom page title" do
-      page.should_not have_selector('title', :text => "#{base_title} | Home")
+      page.should_not have_selector('title', text: "#{base_title} | Home")
     end
 
   end
 
   describe "Help page" do
+    before(:each) do
+      visit help_path
+    end
+
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('h1', :text => 'Help')
+      page.should have_selector('h1', text: 'Help')
     end
     
     it "should have the right title" do
       visit '/static_pages/help'
-      page.should have_selector('title', :text => "#{base_title} | Help")
+      page.should have_selector('title', text: "#{base_title} | Help")
     end
   end
   
   describe "About page" do
+    before(:each) do
+      visit about_path
+    end
+
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'About Us')
+      page.should have_selector('h1', text: 'About Us')
     end
     
     it "should have the right title" do
-      visit '/static_pages/about'
-      page.should have_selector('title', :text => "#{base_title} | About")
+      page.should have_selector('title', text: "#{base_title} | About")
     end
   end
   
   describe "Contact page" do
-    it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
-      page.should have_selector('h1', :text => 'Contact')
+    before(:each) do
+      visit contact_path
+    end
+
+    it "should have the h1 'Contact'" do
+      page.should have_selector('h1', text: 'Contact')
     end
     
-    it "should have the right title" do
-      visit '/static_pages/contact'
-      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Contact")
+    it "should have the h1 'Contact'" do
+      page.should have_selector('title', text: "Ruby on Rails Tutorial Sample App | Contact")
     end
   end
 
