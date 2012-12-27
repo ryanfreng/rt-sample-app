209362cfdb1fe59fe4a4054bfb58c0d16b85faf8
Ryan Freng <ryanfreng@gmail.com>
2012-12-26 22:56:29 -0500

    some update.. unsure what though

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -28,10 +29,17 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
-      # Handle a successful update.
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
     else
       render 'edit'
     end
   end
+
+  private
+    def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
 
 end
